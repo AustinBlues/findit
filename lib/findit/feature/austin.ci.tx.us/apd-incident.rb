914ee3b02f44256aeb95b92bc57f35367a754b3e
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2012-10-04 16:17:33 -0500

    OOPS: RDBI doesn't recognize binds inside quotes.

--- lib/findit/feature/austin.ci.tx.us/apd-incident.rb
+++ lib/findit/feature/austin.ci.tx.us/apd-incident.rb
@@ -36,58 +36,29 @@ module FindIt
         @rectype = nil
                          
         def self.closest(origin)
-	  if false
-	    # Only the latest bike theft - arbitrary, but should change daily.
-            if false
-              fields = 'uid, crime_type, latitude, longitude, date, time, address'
-            else
-              fields = '*'
-            end
-            sql = "SELECT #{fields} FROM austin_ci_tx_us_apd_incident ORDER BY date DESC LIMIT 1"
-	    sth = @db.execute(sql)
-	  else
-            puts "LONG: #{origin.lng}, LAT: #{origin.lat}."
-            begin
-              if true
-                sql = %Q{SELECT *,
-            Distance(geometry, PointFromText('POINT(#{origin.lat} #{origin.lng})', 4326)) AS distance
+          begin
+            sth = @db.execute(%q{SELECT *,
+            Distance(geometry, PointFromText(?, 4326)) AS distance
             FROM austin_ci_tx_us_apd_incident
             ORDER BY distance ASC
             LIMIT 1
-            }
-                puts "SQL: #{sql};"
-                sth = @db.execute(sql)
-              else
-                sth = @db.execute(%q{SELECT *,
-            X(Transform(geometry, 4326)) AS longitude,
-            Y(Transform(geometry, 4326)) AS latitude,
-            Distance(Transform(geometry, 4326), GeomFromText('POINT(? ?)', 4326)) AS distance
-            FROM austin_ci_tx_us_apd_incident
-            ORDER BY distance ASC
-            LIMIT 1
-            }, origin.lat.to_s, origin.lng.to_s)
-              end
-            rescue
-#              puts "SQL: #{@db.last_statement.query};"
-              puts "EXCEPTION(#{__FILE__}: #{__LINE__}): #{$!}."
-            end
-	  end
+            }, "POINT(#{origin.lat} #{origin.lng})")
+          rescue
+            puts "EXCEPTION(#{__FILE__}: #{__LINE__}): #{$!.inspect}."
+          end
+
           rec = sth.fetch[0]	  # FIXME: only using first of potentially many
           sth.finish
 
-          puts "REC: #{rec.inspect}."
-
           return nil unless rec  
           
-          loc = new(FindIt::Location.new(rec[2], rec[3], :DEG),
-            :title => @title,
-            :address => rec[6].capitalize_words,
-            :city => 'Austin',
-            :state => 'TX',
-            :origin => origin
-          )
-          puts "LOC: #{loc.inspect}."
-	  loc
+          new(FindIt::Location.new(rec[2], rec[3], :DEG),
+              :title => @title,
+              :address => rec[6].capitalize_words,
+              :city => 'Austin',
+              :state => 'TX',
+              :origin => origin
+              )
         end
         
         
