ab179c82fb79e9bd1179a78658e726bc2bb833f1
Jeffrey L. Taylor <jeff.taylor@ieee.org>
2012-10-09 10:29:13 -0500

    Correct table name, create geometry column.

--- cycle_nearby.sql
+++ cycle_nearby.sql
@@ -1,4 +1,4 @@
-CREATE TABLE austin_ci_tx_us_incident (
+CREATE TABLE austin_ci_tx_us_apd_incident (
   uid varchar(11) primary key,
   crime_type varchar,
   latitude float,
@@ -7,3 +7,4 @@ CREATE TABLE austin_ci_tx_us_incident (
   time integer,
   address varchar
 );
+SELECT AddGeometryColumn('austin_ci_tx_us_apd_incident', 'the_geom', 4326, 'POINT', 2);
